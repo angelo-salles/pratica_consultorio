839c3191a7665b0d42bff8d96b3515654b7e0193
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-22 08:44:51 -0300

    Esboço das queries para a solução dos exercícios 3, 4 e 5

--- src/main/java/br/com/consultorio/consultorio/Repository/IDiaryRepository.java
+++ src/main/java/br/com/consultorio/consultorio/Repository/IDiaryRepository.java
@@ -1,9 +1,18 @@
 package br.com.consultorio.consultorio.Repository;
 
 import br.com.consultorio.consultorio.Entity.Diary;
+import br.com.consultorio.consultorio.dto.DiaryResultDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Collection;
 
 @Repository
 public interface IDiaryRepository extends JpaRepository<Diary, Long> {
+
+  @Query("")
+  Collection<DiaryResultDTO> findAllAppointmentsDentistInADay(LocalDate day, Long dentistId);
 }
--- src/main/java/br/com/consultorio/consultorio/Repository/ITurnRepository.java
+++ src/main/java/br/com/consultorio/consultorio/Repository/ITurnRepository.java
@@ -1,10 +1,18 @@
 package br.com.consultorio.consultorio.Repository;
 
 import br.com.consultorio.consultorio.Entity.Turn;
+import br.com.consultorio.consultorio.Entity.TurnStatus;
+import br.com.consultorio.consultorio.Entity.TurnStatusEnum;
+import br.com.consultorio.consultorio.dto.TurnResultDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
 
 @Repository
 public interface ITurnRepository extends JpaRepository<Turn, Long> {
 
+  @Query("")
+  Collection<TurnResultDTO> findAllTurnsByStatus(TurnStatusEnum status);
 }
