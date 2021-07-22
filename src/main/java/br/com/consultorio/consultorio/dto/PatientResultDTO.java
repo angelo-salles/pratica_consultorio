7cea037952b5f5d78aaa94f8b32533d6305711db
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-22 08:24:40 -0300

    Criação de um objeto para retornar resultado da query

---
+++ src/main/java/br/com/consultorio/consultorio/dto/PatientResultDTO.java
@@ -0,0 +1,22 @@
+package br.com.consultorio.consultorio.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PatientResultDTO {
+  private Long id;
+  private String name;
+  private String lastName;
+  private String address;
+  private String dni;
+  private LocalDate birthDate;
+  private String phone;
+  private String email;
+
+}
