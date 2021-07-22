b8b29cab04cc9821ef095320f3d67b40fe5b0421
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-22 08:36:53 -0300

    Esboço para o exercício 2

--- src/main/java/br/com/consultorio/consultorio/Repository/IDentistRepository.java
+++ src/main/java/br/com/consultorio/consultorio/Repository/IDentistRepository.java
@@ -1,9 +1,17 @@
 package br.com.consultorio.consultorio.Repository;
 
 import br.com.consultorio.consultorio.Entity.Dentist;
+import br.com.consultorio.consultorio.dto.DentistResultDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.Collection;
 
 @Repository
 public interface IDentistRepository extends JpaRepository<Dentist, Long> {
+
+  @Query("")
+  Collection<DentistResultDTO> findAllDentistsWithMoreThanOneAppointmentInADay(LocalDate day);
 }
